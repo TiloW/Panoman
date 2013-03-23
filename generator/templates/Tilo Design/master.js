f87b8ba2f7a4eecf1a9cf2d94cf030473d820a11
Tilo Wiedera <tilo@wiedera.de>
2013-03-23 12:22:41 +0100

    new sample design

---
+++ generator/templates/Tilo Design/master.js
@@ -0,0 +1,25 @@
+var updateSize = function() {
+  innerDist = $('#grid ul').outerWidth() - $('#grid ul').width();
+  width = $('#grid').parent().innerWidth() - innerDist;
+  singleWidth = $('#grid li').outerWidth(true);
+  count = Math.floor(width / singleWidth);
+  
+  dist = width - singleWidth*count;
+  
+  if(dist < 60)
+    dist += singleWidth;
+    
+  singleDist = Math.floor(dist/2) + 'px';
+  
+  $('header, footer').css('width', ($('header').outerWidth(true) - dist) + 'px');
+    
+  $('#grid, header, footer').css({
+    'margin-left': singleDist,
+    'margin-right': singleDist
+  });
+};
+
+$(function() {
+  $(window).on('resize', updateSize);
+  updateSize();
+});
